06105b0fd7cd2eb076e63899893bc01ae06c068c
mcruzr85 <mcruzrodriguez1985@gmail.com>
2022-11-14 16:16:02 -0300

    homework Estructura de Datos II y III done!

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -11,9 +11,94 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
 
-function Node(value) {}
+function LinkedList() {
+   this.head = null;
+   this._length = 0;
+}
+
+LinkedList.prototype.add = function(value){
+  let node = new Node(value);
+  let actual = this.head;
+
+  if(!actual){  //lista vacia    
+    this.head = node;
+    this._length++;
+    return node;
+  }
+   while(actual.next){ //buscando el ultimo nodo
+    actual = actual.next;
+   }
+   actual.next = node;  //el puntero del ultimo nodo apunta al nuevo nodo
+   this._length ++;
+   return node;
+}
+
+LinkedList.prototype.remove = function(){
+  let actual = this.head;
+  let value;
+  if(!actual) return null;  //si esta vacia
+  if(!actual.next){   //si tiene un nodo
+    value = actual.value;
+    this.head = null
+    this._length = 0;        
+    }
+  else{
+    while(actual.next.next){
+      actual = actual.next;
+    }
+    value = actual.next.value;
+    actual.next = null;
+    this._length--;
+  }
+       return value;      
+};
+
+LinkedList.prototype.search = function(data){
+  let actual = this.head;
+  let isAFunction = false;
+
+  if(typeof data === 'function'){
+    isAFunction = true;
+  }
+  while(actual){
+    if(isAFunction){
+          if(data(actual.value)) return actual.value;
+      //else actual = actual.next;
+     }else if(actual.value === data) return actual.value;
+     actual = actual.next;
+    }
+    return null;
+  };
+     
+ 
+  
+function par(arg){
+  return (arg%2 === 0);
+}
+
+
+
+let myList = new LinkedList();
+myList.add(1);
+myList.add(3);
+myList.add(6);
+//myList.add('Luis')
+console.log(myList);
+var a = myList.search(par);
+console.log(a);
+a = myList.remove();
+console.log(a);
+a = myList.remove();
+console.log(a);
+a = myList.remove();
+console.log(a);
+console.log(myList);
+
 
 /*
 Implementar la clase HashTable.
--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,80 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinarySearchTree.prototype.size = function(){
+  if((this.left) && (this.right))
+      return 1 + this.left.size() + this.right.size();
+  else if ((this.left === null) && (this.right))
+      return 1 + this.right.size();
+  else if ((this.left) && (this.right === null))
+      return 1 + this.left.size();
+  else return 1;
+}
+
+BinarySearchTree.prototype.insert = function(value){
+  if(value < this.value){
+    if(this.left === null){
+      let newArbol = new BinarySearchTree(value);
+      this.left = newArbol;
+    }else this.left.insert(value);    
+  }else{
+    if(this.right === null){
+        let newArbol = new BinarySearchTree(value); 
+        this.right = newArbol;
+    }else this.right.insert(value);
+  }  
+};
+
+BinarySearchTree.prototype.contains = function(value){
+  if(this.value === value) return true;
+  if(value > this.value){
+      if(this.right === null) return false;
+      else return this.right.contains(value);
+    }
+  if(value < this.value){
+      if(this.left === null) return false;
+      else return this.left.contains(value); 
+    } 
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, order){
+  if(order === 'pre-order'){
+    //root - izq -derecha
+   cb(this.value);    
+   if(this.left !== null) this.left.depthFirstForEach(cb, order); 
+   if(this.right !== null) this.right.depthFirstForEach(cb, order);
+ 
+   }else if(order === 'post-order'){
+    //izq - der --root
+   if(this.left !== null) this.left.depthFirstForEach(cb, order); 
+   if(this.right !== null) this.right.depthFirstForEach(cb, order);
+   cb(this.value);  
+
+   }else{
+    //in-order   izq-root-derecha
+    if(this.left !== null) this.left.depthFirstForEach(cb, order); 
+    cb(this.value);
+    if(this.right !== null) this.right.depthFirstForEach(cb, order);
+   }    
+};
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, arr){ 
+      if(! arr){
+        var arr = [];
+      }
+      cb(this.value);
+      this.left && arr.push(this.left);
+      this.right && arr.push(this.right);
+
+      arr.length && arr.shift().breadthFirstForEach(cb, arr);
+      return arr;
+    };
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
